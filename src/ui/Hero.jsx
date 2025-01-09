df5a691b2173d2ccdde3783dacdec301658ef878
riddsep <ridoseptiawan00@gmail.com>
2025-01-09 11:06:54 +0700

    minor changes

--- src/ui/Hero.jsx
+++ src/ui/Hero.jsx
@@ -2,22 +2,22 @@ import Navbar from "./Navbar";
 
 function Hero() {
   return (
-    <div className="relative h-dvh w-full bg-gradient bg-cover bg-center bg-no-repeat text-white">
+    <div className="relative h-dvh w-full bg-gradient bg-cover bg-center bg-no-repeat text-white md:pt-5">
       <Navbar />
-      <div className="relative z-20 w-full max-w-[500px] overflow-hidden px-5 sm:px-10 md:max-w-none lg:mx-auto lg:max-w-6xl lg:px-0 lg:pt-10">
+      <div className="relative z-20 w-full max-w-[500px] overflow-hidden px-5 sm:px-10 md:max-w-none lg:mx-auto lg:max-w-6xl lg:pt-10">
         <h2 className="mb-5 text-3xl font-semibold sm:text-4xl md:text-5xl">
           rido septiawan.
         </h2>
         <h1 className="mb-5 text-6xl font-bold sm:text-7xl md:mb-0">
           a{" "}
-          <span className="relative before:absolute before:-bottom-1 before:left-0 before:h-2 before:w-full before:-skew-y-1 before:bg-green-800">
+          <span className="relative before:absolute before:-bottom-1 before:left-0 before:h-2 before:w-full before:-skew-y-1 before:bg-[#17CF97]">
             passionate
           </span>{" "}
           frontend developer
         </h1>
         <p className="mb-5 w-3/4 text-xl font-semibold sm:w-[480px] sm:text-2xl md:mb-10 md:w-full md:text-7xl">
           creating exceptional web experiences and inspiring tech{" "}
-          <span className="decoration-green-800 decoration-wavy underline-offset-4 lg:underline">
+          <span className="decoration-[#17CF97] decoration-wavy underline-offset-4 lg:underline">
             growth.
           </span>
         </p>
