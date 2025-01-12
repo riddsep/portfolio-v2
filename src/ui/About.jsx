b1b93f95c889e80f32d2f2e94e481f7f68a20a6d
riddsep <ridoseptiawan00@gmail.com>
2025-01-12 13:38:38 +0700

    feat: footer

--- src/ui/About.jsx
+++ src/ui/About.jsx
@@ -3,10 +3,18 @@ function About() {
     <div className="bg-dust">
       <div className="flex flex-col gap-5 px-5 py-10 text-white sm:px-10 md:flex-row md:py-16 lg:mx-auto lg:max-w-6xl xl:px-0">
         <div className="max-w-[300px] lg:max-w-[600px]">
-          <h1 className="highlight mb-6 text-3xl font-bold sm:text-4xl md:text-5xl lg:mb-10 lg:text-6xl">
+          <h1
+            className="highlight mb-6 text-3xl font-bold sm:text-4xl md:text-5xl lg:mb-10 lg:text-6xl"
+            data-aos="fade-right"
+            data-aos-delay="200"
+          >
             About me
           </h1>
-          <p className="mb-4 md:mb-6 md:text-xl">
+          <p
+            className="mb-4 md:mb-6 md:text-xl"
+            data-aos="fade-right"
+            data-aos-delay="200"
+          >
             I am a bachelor&apos;s graduate in Informatics and an alumnus of an
             online frontend developer bootcamp. While pursuing my degree, I
             worked as an online driver and housekeeping staff. After a journey
@@ -15,13 +23,21 @@ function About() {
             Corp.
           </p>
 
-          <p className="max-w-[450px] md:text-xl">
+          <p
+            className="max-w-[450px] md:text-xl"
+            data-aos="fade-right"
+            data-aos-delay="200"
+          >
             <span className="highlight font-semibold">Let’s connect </span> if
             you’re interested in collaborating or discussing web development!
           </p>
         </div>
-        <div className="relative mx-auto max-w-96 after:absolute after:-left-52 after:bottom-14 after:hidden after:content-[url('/arrow-2.svg')] lg:after:inline-block">
-          <div className="relative before:absolute before:-right-32 before:top-4 before:hidden before:-rotate-12 before:content-[url('/arrow.svg')] lg:before:inline-block">
+        <div
+          className="relative mx-auto max-w-96 after:absolute after:-left-52 after:bottom-14 after:hidden after:content-[url('/arrow-2.svg')] lg:after:inline-block"
+          data-aos="fade-left"
+          data-aos-delay="200"
+        >
+          <div className="relative before:absolute before:-right-32 before:top-4 before:hidden before:-rotate-12 before:overflow-hidden before:content-[url('/arrow.svg')] lg:before:inline-block">
             <img src="/aboutme.png" alt="" className="mx-auto w-full" />
           </div>
         </div>
--- src/ui/ProjectList.jsx
+++ src/ui/ProjectList.jsx
@@ -6,11 +6,19 @@ function ProjectList() {
     <div className="bg-dust text-white">
       <TechnologyIcons />
       <div className="px-5 pb-20 sm:px-10 lg:mx-auto lg:max-w-6xl xl:px-0">
-        <h1 className="highlight mb-6 text-3xl font-bold sm:text-4xl md:text-5xl lg:mb-10 lg:text-6xl">
-          <span className="relative"> Featured Case Studies</span>
+        <h1
+          className="highlight mb-6 text-3xl font-bold sm:text-4xl md:text-5xl lg:mb-10 lg:text-6xl"
+          data-aos="fade-right"
+          data-aos-delay="200"
+        >
+          <span className="relative">Featured Case Studies</span>
         </h1>
 
-        <p className="mb-20 w-full sm:text-xl md:max-w-[600px] md:text-xl lg:mb-24 lg:text-2xl">
+        <p
+          className="mb-20 w-full sm:text-xl md:max-w-[600px] md:text-xl lg:mb-24 lg:text-2xl"
+          data-aos="fade-right"
+          data-aos-delay="200"
+        >
           Learn how I’ve applied design and development principles to solve
           problems and deliver solutions.
         </p>
